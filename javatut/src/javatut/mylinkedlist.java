a885a632b4d4cbb5b8565553e729b125b5c001f3
sureshbhabugl <sureshbhabu.gl@gmail.com>
2018-01-22 17:35:06 -0800

    sc

--- javatut/src/javatut/mylinkedlist.java
+++ javatut/src/javatut/mylinkedlist.java
@@ -1,10 +1,14 @@
 package javatut;
+import java.util.*;
 
 public class mylinkedlist {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Implement Linked list functionality
+		
+		LinkedList <String> ll = new LinkedList<String>();
+		
 	}
 
 }
